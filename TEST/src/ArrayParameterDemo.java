a1f635a9da9e8483044d3dccec0267463394b311
unul09 <39664609+unul09@users.noreply.github.com>
2021-07-31 12:43:37 +0900

    배열의 전달

---
+++ TEST/src/ArrayParameterDemo.java
@@ -0,0 +1,12 @@
+
+public class ArrayParameterDemo {
+
+	public static void main(String[] args) {
+		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','.'};
+		ArrayParameter.printCharArray(c);
+		ArrayParameter.replaceSpace(c);
+		ArrayParameter.printCharArray(c);;
+
+	}
+
+}
