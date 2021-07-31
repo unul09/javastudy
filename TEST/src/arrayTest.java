d9bf80201f5562ecdf5c060f5084841dd96f5b8e
unul09 <39664609+unul09@users.noreply.github.com>
2021-07-31 10:40:51 +0900

    입력오류 예외처리

--- TEST/src/arrayTest.java
+++ TEST/src/arrayTest.java
@@ -1,17 +1,29 @@
 import java.util.Scanner;
-public class arrayTest { //string array, for-each
-	
-	
-	public static void main(String[] args) {		
+import java.util.InputMismatchException;
+
+public class arrayTest { // 입력오류 예외처리
+
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String name[] = new String[3];
-		
-		for(String n : name) {
-			n = sc.next();
-			System.out.println(n);
+
+		System.out.println("정수를 입력해 주세요. -1 입력시 프로그램 종료");
+
+		while (true) {
+			try {
+				System.out.print(">");
+				int a = sc.nextInt();
+				if (a == -1) {
+					System.out.println("프로그램 종료");
+					break;
+				}
+			} catch (InputMismatchException e) {
+				System.out.println("정수가 아닙니다! 다시 입력해주세요");
+				sc.next();
+			}
 		}
+
 		sc.close();
-			
+
 	}
 
 }
