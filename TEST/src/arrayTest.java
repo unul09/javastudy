ddee50c2b99058ee1c16aad7244fee21c24fdc04
unul09 <39664609+unul09@users.noreply.github.com>
2021-07-31 10:20:22 +0900

    0으로 나눌 때 발생하는 ArithmeticException 예외 처리(직접)

--- TEST/src/arrayTest.java
+++ TEST/src/arrayTest.java
@@ -3,23 +3,23 @@ public class arrayTest { //0으로 나눌 때 발생하는 ArithmeticException 예외 처리
 	
 	
 	public static void main(String[] args) {		
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		while(true) {
-		System.out.print("나뉨수를 입력하시오:");
-		int dividend = scanner.nextInt(); // 나뉨수 입력
-		System.out.print("나눗수를 입력하시오:");
-		int divisor = scanner.nextInt(); // 나눗수 입력
-		try {
-		System.out.println(dividend + "를 "+ divisor + "로 나누면 몫은 " + dividend/divisor + "입니다.");
-		break; // 정상적인 나누기 완료 후 while 벗어나기
-		}
-		catch(ArithmeticException e) { // ArithmeticException 예외 처리 코드
-		System.out.println("0으로 나눌 수 없습니다! 다시 입력하세요");
-		}
-		}
-		scanner.close();
+			System.out.print("나뉨수를 입력하시오:");
+			int dividend = sc.nextInt(); // 나뉨수 입력
+			System.out.print("나눗수를 입력하시오:");
+			int divisor = sc.nextInt(); // 나눗수 입력
+			
+			try {
+				System.out.println(dividend+"를 "+divisor+"로 나눈 값은 "+dividend/divisor+"입니다.");
+				break;
+			}
+			catch(ArithmeticException e) {
+				System.out.println("0으로 나누지 마시오! 다시.");
+			}
 
-
+		}
+			
 	}
 
 }
