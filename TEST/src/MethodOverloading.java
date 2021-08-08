f9b050828358fe70e5a5870d03a36e80a3ac0f6c
unul09 <39664609+unul09@users.noreply.github.com>
2021-08-08 18:18:29 +0900

    오버로딩 getSum

---
+++ TEST/src/MethodOverloading.java
@@ -0,0 +1,16 @@
+
+public class MethodOverloading {
+	
+	public int getSum(int i, int j) {
+		return i+j;
+	}
+	
+	public int getSum(int i, int j, int k) {
+		return i+j+k;
+	}
+	
+	public double getSum(double i, double j) {
+		return i+j;
+	}
+
+}
---
+++ TEST/src/MethodOverloadingEx.java
@@ -0,0 +1,18 @@
+
+public class MethodOverloadingEx {
+
+	public static void main(String[] args) {
+		
+		MethodOverloading a = new MethodOverloading();
+		
+		int i = a.getSum(1, 2);
+		int j = a.getSum(1, 2, 3);
+		double k = a.getSum(1.7, 2.1);
+		
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+
+	}
+
+}
